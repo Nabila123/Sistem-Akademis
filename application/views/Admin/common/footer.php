530c506800d5e88551ad629065a5bbec93bef23d
Muhamad_N <nabilmunawar5@gmail.com>
2018-07-09 15:09:40 +0700

    Sistem Akademis

---
+++ application/views/Admin/common/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <p class="copyright pull-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+            <a href="#">Nabil's Creative Tim</a>, <span style="font-size: 1.5rem">Made With <i class="tiny material-icons">favorite</i> In Kost</span>
+        </p>
+    </div>
+</footer>
